0f106c118c335dd8bb39f0badea719fb8537ee3a
reeveress <zuhraa470@gmail.com>
2018-03-21 16:47:02 -0700

    updated 3/21/18

--- wr_expect.py
+++ wr_expect.py
@@ -7,7 +7,7 @@ import time
 parser = argparse.ArgumentParser(description = 'This script uses expect to obtain status information from a White Rabbit endpoint. It takes the IP address of the white rabbit endpoint as a string and pushes the status information into redis at time intervals specified by -t flag, default is 300.',
                                     formatter_class = argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('ip_addr', action = 'store', help = 'Specify the IP address of the White Rabbit endpoint (i.e. x.x.x.x).')
-parser.add_argument('node_id',action='store', help='Specify the node ID number (int from 0 to 29) to get the corresponding Redis data')
+parser.add_argument('node',action='store', help='Specify the node ID number (int from 0 to 29) to get the corresponding Redis data')
 parser.add_argument('-t', action = 'store', dest = 'interval', default = 300, help = 'Specify the time interval, in seconds, for data collection')
 
 
@@ -26,6 +26,6 @@ while True:
     start_index = re.search("temp: ", stat).end()
     temp_wr = stat[start_index:start_index+7]
     # Push the status info to Redis database
-    r.hset('status:node:%d'%int(args.node_id),'temp_wr',temp_wr)
+    r.hset('status:node:%d'%int(args.node),'temp_wr',temp_wr)
     print(temp_wr)
     time.sleep(int(args.interval))
